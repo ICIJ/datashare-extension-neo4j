fb8eb4763286af163a4d6787190d2afe7a6fdb3b
Clément Doumouro <clement.doumouro@gmail.com>
2023-02-08 12:29:18 +0100

    chore:: test dependencies caching

--- neo4j-app/neo4j_app/app/dependencies.py
+++ neo4j-app/neo4j_app/app/dependencies.py
@@ -7,12 +7,12 @@ from neo4j_app.core import AppConfig
 from neo4j_app.core.elasticsearch import ESClient
 
 
-def get_global_config() -> AppConfig:
+def get_global_config_dep() -> AppConfig:
     return AppConfig.get_global_config()
 
 
 async def neo4j_driver_dep(
-    config: AppConfig = Depends(get_global_config),
+    config: AppConfig = Depends(get_global_config_dep),
 ) -> AsyncGenerator[neo4j.AsyncSession, None]:
     driver = config.to_neo4j_driver()
     try:
@@ -31,7 +31,7 @@ async def neo4j_session_dep(
 
 
 async def es_client_dep(
-    config: AppConfig = Depends(get_global_config),
+    config: AppConfig = Depends(get_global_config_dep),
 ) -> AsyncGenerator[ESClient, None]:
     async with config.to_es_client() as client:
         yield client
