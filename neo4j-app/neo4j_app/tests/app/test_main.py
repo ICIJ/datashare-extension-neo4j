c16dbdc6e7f9c0373b36e95174ed4f5291c1f392
Clément Doumouro <clement.doumouro@gmail.com>
2023-02-15 12:18:42 +0100

    feat: add a route to get the app configuration

--- neo4j-app/neo4j_app/tests/app/test_main.py
+++ neo4j-app/neo4j_app/tests/app/test_main.py
@@ -1,12 +1,31 @@
+import pytest
 from starlette.testclient import TestClient
+
+from neo4j_app.core import AppConfig
 
 
 def test_ping(test_client: TestClient):
     # Given
-    url = "ping"
+    url = "/ping"
 
     # When
     res = test_client.get(url)
 
     # Then
     assert res.status_code == 200, res.json()
+    assert res.content == b"pong"
+
+
+def test_config(test_client: TestClient):
+    # Given
+    url = "/config"
+
+    # When
+    res = test_client.get(url)
+
+    # Then
+    assert res.status_code == 200, res.json()
+    try:
+        AppConfig.parse_obj(res.json())
+    except:
+        pytest.fail(f"Failed to parse response as a {AppConfig.__name__}")
