b4569f27ae2b45706bed23072053997e3d1078b1
Clément Doumouro <clement.doumouro@gmail.com>
2023-03-02 11:49:38 +0100

    feat: add a `GET /version` in the Python application

--- neo4j-app/neo4j_app/app/main.py
+++ neo4j-app/neo4j_app/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 
 from neo4j_app.app.dependencies import get_global_config_dep
 from neo4j_app.core import AppConfig
@@ -16,5 +16,11 @@ def main_router() -> APIRouter:
     @router.get("/config")
     def config(config: AppConfig = Depends(get_global_config_dep)) -> AppConfig:
         return config
+
+    @router.get("/version")
+    def version() -> Response:
+        import neo4j_app
+
+        return Response(content=neo4j_app.__version__, media_type="text/plain")
 
     return router
