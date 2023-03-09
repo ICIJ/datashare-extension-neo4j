1b2e816bf6abc89ab71a74d6478b1d115efd0db9
Clément Doumouro <clement.doumouro@gmail.com>
2023-03-09 18:14:43 +0100

    chore: add uniqueness constraint on documents and named entities ids

--- neo4j-app/neo4j_app/core/neo4j/migrations/migrations.py
+++ neo4j-app/neo4j_app/core/neo4j/migrations/migrations.py
@@ -1,6 +1,13 @@
 import neo4j
 
-from neo4j_app.constants import MIGRATION_NODE, MIGRATION_VERSION
+from neo4j_app.constants import (
+    DOC_ID,
+    DOC_NODE,
+    MIGRATION_NODE,
+    MIGRATION_VERSION,
+    NE_ID,
+    NE_NODE,
+)
 
 
 async def create_migration_unique_constraint_tx(tx: neo4j.AsyncTransaction):
@@ -10,3 +17,18 @@ FOR (m:{MIGRATION_NODE})
 REQUIRE (m.{MIGRATION_VERSION}) IS UNIQUE
 """
     await tx.run(constraint_query)
+
+
+async def create_document_and_ne_id_unique_constraint_tx(tx: neo4j.AsyncTransaction):
+    doc_query = f"""CREATE CONSTRAINT constraint_document_unique_id
+IF NOT EXISTS 
+FOR (doc:{DOC_NODE})
+REQUIRE (doc.{DOC_ID}) IS UNIQUE
+"""
+    await tx.run(doc_query)
+    ne_query = f"""CREATE CONSTRAINT constraint_named_entity_unique_id
+IF NOT EXISTS 
+FOR (ent:{NE_NODE})
+REQUIRE (ent.{NE_ID}) IS UNIQUE
+"""
+    await tx.run(ne_query)
