a212a505114718201a20f63673fbf86355e0d4a1
Clément Doumouro <clement.doumouro@gmail.com>
2023-03-23 14:35:38 +0100

    fix: MERGE import statements

--- neo4j-app/neo4j_app/core/neo4j/named_entities.py
+++ neo4j-app/neo4j_app/core/neo4j/named_entities.py
@@ -27,28 +27,16 @@ WITH row, \
 [offset IN split(row.{NE_OFFSETS}, '{NE_OFFSET_SPLITCHAR}') | toInteger(offset)] \
 as rowOffsets  
 MERGE (mention:{NE_NODE} {{{NE_ID}: row.{NE_ID}}})
-ON CREATE
-    SET
-        mention.{NE_CATEGORY} = row.{NE_CATEGORY},
-        mention.{NE_DOC_ID} = row.{NE_DOC_ID},
-        mention.{NE_EXTRACTOR} = row.{NE_EXTRACTOR},
-        mention.{NE_EXTRACTOR_LANG} = row.{NE_EXTRACTOR_LANG},
-        mention.{NE_MENTION} = row.{NE_MENTION},
-        mention.{NE_MENTION_NORM} = row.{NE_MENTION_NORM},
-        mention.{NE_MENTION_NORM_TEXT_LENGTH} =\
-            toInteger(row.{NE_MENTION_NORM_TEXT_LENGTH}),
-        mention.{NE_OFFSETS} = rowOffsets
-ON MATCH
-    SET 
-        mention.{NE_CATEGORY} = row.{NE_CATEGORY},
-        mention.{NE_DOC_ID} = row.{NE_DOC_ID},
-        mention.{NE_EXTRACTOR} = row.{NE_EXTRACTOR},
-        mention.{NE_EXTRACTOR_LANG} = row.{NE_EXTRACTOR_LANG},
-        mention.{NE_MENTION} = row.{NE_MENTION},
-        mention.{NE_MENTION_NORM} = row.{NE_MENTION_NORM},
-        mention.{NE_MENTION_NORM_TEXT_LENGTH} =\
-            toInteger(row.{NE_MENTION_NORM_TEXT_LENGTH}),
-        mention.{NE_OFFSETS} = rowOffsets
+SET
+    mention.{NE_CATEGORY} = row.{NE_CATEGORY},
+    mention.{NE_DOC_ID} = row.{NE_DOC_ID},
+    mention.{NE_EXTRACTOR} = row.{NE_EXTRACTOR},
+    mention.{NE_EXTRACTOR_LANG} = row.{NE_EXTRACTOR_LANG},
+    mention.{NE_MENTION} = row.{NE_MENTION},
+    mention.{NE_MENTION_NORM} = row.{NE_MENTION_NORM},
+    mention.{NE_MENTION_NORM_TEXT_LENGTH} =\
+        toInteger(row.{NE_MENTION_NORM_TEXT_LENGTH}),
+    mention.{NE_OFFSETS} = rowOffsets
 """
     res = await tx.run(query)
     summary = await res.consume()
