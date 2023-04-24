008205615e7b1e0f42d706dc973b889b4ac4697e
Clément Doumouro <clement.doumouro@gmail.com>
2023-04-24 14:42:38 +0200

    fix: export directory prefix

--- src/main/java/org/icij/datashare/Neo4jResource.java
+++ src/main/java/org/icij/datashare/Neo4jResource.java
@@ -339,7 +339,7 @@ public class Neo4jResource implements AutoCloseable {
         String database = "neo4j";
         // Define a temp dir
         Path tmpDir = createTempDirectory(
-            Path.of(FileSystems.getDefault().getSeparator(), "tmp"), "neo4j-export");
+            Path.of(FileSystems.getDefault().getSeparator(), "tmp"), "neo4j-export-");
         org.icij.datashare.Objects.Neo4jAppNeo4jCSVRequest neo4jAppRequest = request.toNeo4j(
             tmpDir.toAbsolutePath().toString());
         try {
@@ -350,7 +350,7 @@ public class Neo4jResource implements AutoCloseable {
             InputStream is = new FileInputStream(res.path);
             return new BufferedInputStream(is);
         } finally {
-            Files.delete(tmpDir);
+            Files.deleteIfExists(tmpDir);
         }
     }
     //CHECKSTYLE.ON: AbbreviationAsWordInName
