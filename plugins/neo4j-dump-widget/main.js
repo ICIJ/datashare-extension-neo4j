0020df35aaef27d6b5d3e81b6acf04d11312c33f
Clément Doumouro <clement.doumouro@gmail.com>
2023-08-04 13:42:08 +0200

    chore: move widget to the insight page

--- plugins/neo4j-dump-widget/main.js
+++ plugins/neo4j-dump-widget/main.js
@@ -1,8 +1,15 @@
-import WidgetHook from "./components/WidgetHook.vue"
+import WidgetHook from './components/WidgetHook.vue'
+import dumpWidgetFactory from './core/WidgetNeo4jDump'
 
 document.addEventListener('datashare:ready', async ({ detail }) => {
   detail.core.registerHook({
     target: 'landing.form.project:before',
     definition: WidgetHook
+  })
+  detail.core.registerWidget({
+    name: 'WidgetNeo4jDump',
+    type(WidgetEmptyCls) {
+      return dumpWidgetFactory(WidgetEmptyCls)
+    }
   })
 }, false)
