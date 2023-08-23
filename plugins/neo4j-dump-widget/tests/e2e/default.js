1c19e41aab217c29e13472a31fdbef1f5e3481af
Clément Doumouro <clement.doumouro@gmail.com>
2023-08-23 16:20:02 +0200

    chore: empty e2e test

--- plugins/neo4j-dump-widget/tests/e2e/default.js
+++ plugins/neo4j-dump-widget/tests/e2e/default.js
@@ -3,11 +3,6 @@ const { main } = require('../../package.json')
 module.exports = {
   'default configuration test' : function (browser) {
     browser.url('http://localhost:8008')
-    // Wait for the app to be started
-    browser.waitForElementVisible('.landing')
-    browser.assert.titleContains('Datashare')
-    browser.expect.element(`script[src="/plugins/package/${main}"]`).to.be.present
-    browser.expect.element('.landing__plugin').to.be.present
-    browser.expect.element('.landing__plugin').text.to.startWith('This alert was created by `datashare-plugin-neo4j-dump-widget` plugin.')
+    // TODO...
   }
 };
