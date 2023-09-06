53acc25699e8fc5c1601a3e5d8146bdb47c72c6d
Clément Doumouro <clement.doumouro@gmail.com>
2023-09-06 12:02:54 +0200

    chore: fixes after review

--- plugins/neo4j-dump-widget/main.js
+++ plugins/neo4j-dump-widget/main.js
@@ -3,12 +3,12 @@ import { default as neo4jStoreBuilder } from './store/Neo4jModule'
 import { CoreExtension } from './core/CoreExtension'
 
 document.addEventListener('datashare:ready', async ({ detail }) => {
-  if (!detail.core._store.hasModule('neo4j')) {
+  if (!detail.core.store.hasModule('neo4j')) {
     const extension = new CoreExtension(detail.core)
     // Register the extension under this.$neo4jCore
     extension.useExtension()
     const module = neo4jStoreBuilder(extension)
-    detail.core._store.registerModule('neo4j', module)
+    detail.core.store.registerModule('neo4j', module)
   }
   detail.core.registerWidget({
     name: 'WidgetNeo4jDump',
