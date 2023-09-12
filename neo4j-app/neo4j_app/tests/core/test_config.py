7ff90b73332c20fb7be7055e2870d84388b8dd08
Clément Doumouro <clement.doumouro@gmail.com>
2023-09-12 14:13:39 +0200

    chore: test forwarding of page size from config to client

--- neo4j-app/neo4j_app/tests/core/test_config.py
+++ neo4j-app/neo4j_app/tests/core/test_config.py
@@ -13,7 +13,6 @@ def test_should_support_alias():
     neo4j_app_name = "test_name"
     config = AppConfig(
         neo4j_app_name=neo4j_app_name,
-        neo4j_project="test-project",
     )
 
     # Then
@@ -30,7 +29,6 @@ neo4jImportDir=import-dir
             AppConfig(
                 neo4j_app_host="127.0.0.1",
                 neo4j_app_port=8080,
-                neo4j_project="test-project",
                 elasticsearch_address="http://127.0.0.1:9200",
             ),
         ),
@@ -45,7 +43,6 @@ someExtraInfo=useless
             AppConfig(
                 neo4j_app_host="this-the-neo4j-app",
                 neo4j_app_port=3333,
-                neo4j_project="test-project",
                 elasticsearch_address="http://elasticsearch:9222",
             ),
         ),
@@ -66,18 +63,11 @@ def test_should_load_from_java(config: str, expected_config: AppConfig):
     "config,expected_uvicorn_config",
     [
         (
-            AppConfig(
-                neo4j_app_host="127.0.0.1",
-                neo4j_app_port=8888,
-                neo4j_project="test-project",
-            ),
+            AppConfig(neo4j_app_host="127.0.0.1", neo4j_app_port=8888),
             UviCornModel(host="127.0.0.1", port=8888),
         ),
         (
-            AppConfig(
-                neo4j_app_log_level="DEBUG",
-                neo4j_project="test-project",
-            ),
+            AppConfig(neo4j_app_log_level="DEBUG"),
             UviCornModel(host="127.0.0.1", port=8080),
         ),
     ],
@@ -90,16 +80,27 @@ def test_to_uvicorn(config: AppConfig, expected_uvicorn_config: UviCornModel):
     assert uvicorn_config == expected_uvicorn_config
 
 
-@pytest.mark.pull(id="")
+@pytest.mark.pull(id="62")
 def test_should_support_address_without_port():
     # Given
-    config = AppConfig(
-        elasticsearch_address="http://elasticsearch",
-        neo4j_project="test-project",
-    )
+    config = AppConfig(elasticsearch_address="http://elasticsearch")
     # Then
     with fail_if_exception("Failed to initialize ES client"):
         config.to_es_client()
+
+
+@pytest.mark.pull(id="91")
+def test_should_forward_page_size_to_client():
+    # Given
+    es_default_page_size = 666
+    config = AppConfig(
+        elasticsearch_address="http://elasticsearch",
+        es_default_page_size=es_default_page_size,
+    )
+    # When
+    client = config.to_es_client()
+    # Then
+    assert client.pagination_size == es_default_page_size
 
 
 @pytest.mark.parametrize("user,password", [(None, "somepass"), ("someuser", None)])
@@ -111,7 +112,6 @@ def test_should_raise_for_missing_auth_part(
     with pytest.raises(ValidationError, match=expected_msg):
         AppConfig(
             elasticsearch_address="http://elasticsearch:9222",
-            neo4j_project="test-project",
             neo4j_user=user,
             neo4j_password=password,
         )
