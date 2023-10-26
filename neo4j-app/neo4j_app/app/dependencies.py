4154b4a80a5618e4eb0d8e2002f77c8d34c209b5
Clément Doumouro <clement.doumouro@gmail.com>
2023-10-26 17:47:44 +0200

    fix: `neo4j` enterprise support detection

--- neo4j-app/neo4j_app/app/dependencies.py
+++ neo4j-app/neo4j_app/app/dependencies.py
@@ -69,7 +69,7 @@ async def neo4j_driver_enter(**__):
     await _NEO4J_DRIVER.__aenter__()  # pylint: disable=unnecessary-dunder-call
 
     logger.debug("pinging neo4j...")
-    async with registry_db_session(_NEO4J_DRIVER) as sess:
+    async with _NEO4J_DRIVER.session(database=neo4j.SYSTEM_DATABASE) as sess:
         await sess.run("CALL db.ping()")
     logger.debug("neo4j driver is ready")
 
@@ -254,6 +254,7 @@ FASTAPI_LIFESPAN_DEPS = [
     (config_enter, None),
     (loggers_enter, None),
     (neo4j_driver_enter, neo4j_driver_exit),
+    (create_project_registry_db_enter, None),
     (es_client_enter, es_client_exit),
     (test_process_manager_enter, test_process_manager_exit),
     (test_db_path_enter, test_db_path_exit),
@@ -261,7 +262,6 @@ FASTAPI_LIFESPAN_DEPS = [
     (task_task_manager_enter, None),
     (event_publisher_enter, None),
     (worker_pool_enter, worker_pool_exit),
-    (create_project_registry_db_enter, None),
     (migrate_app_db_enter, None),
 ]
 
