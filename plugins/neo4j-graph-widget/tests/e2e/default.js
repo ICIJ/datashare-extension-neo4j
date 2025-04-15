2d83aedb72ab1bee325b7b03ba05be685ba930ca
Pierre Romera <hello@pirhoo.com>
2025-04-15 09:52:21 +0000

    refactor: remove unused e2e test icij/datashare#1701

--- plugins/neo4j-graph-widget/tests/e2e/default.js
+++
@@ -1,8 +0,0 @@
-const { main } = require('../../package.json')
-
-module.exports = {
-  'default configuration test' : function (browser) {
-    browser.url('http://localhost:8008')
-    // TODO...
-  }
-};
