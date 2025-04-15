d65bb57e6b6da2d5383752b4de311e2d9d6847b2
Pierre Romera <hello@pirhoo.com>
2025-04-15 09:49:45 +0000

    refactor: remove extention core installation  in favor of composable icij/datashare#1701

--- plugins/neo4j-graph-widget/main.js
+++ plugins/neo4j-graph-widget/main.js
@@ -1,20 +1,14 @@
-import graphWidgetFactory from './core/WidgetNeo4jGraph'
-import { default as neo4jStoreBuilder } from './store/Neo4jModule'
-import { CoreExtension } from './core/CoreExtension'
+import defineWidgetNeo4jGraph from './core/defineWidgetNeo4j'
 
-document.addEventListener('datashare:ready', async ({ detail }) => {
-  if (!detail.core.store.hasModule('neo4j')) {
-    const extension = new CoreExtension(detail.core)
-    // Register the extension under this.$neo4jCore
-    extension.useExtension()
-    const module = neo4jStoreBuilder(extension)
-    detail.core.store.registerModule('neo4j', module)
-  }
-  detail.core.registerWidget({
-    name: 'WidgetNeo4jGraph',
-    type(WidgetEmptyCls) {
-      return graphWidgetFactory(WidgetEmptyCls)
-    },
-    order: 36,
-  })
-}, false)
+document.addEventListener(
+  'datashare:ready',
+  async ({ detail }) => {
+    detail.core.registerWidget({
+      name: 'WidgetNeo4j',
+      section: 'graph',
+      order: 0,
+      type: defineWidgetNeo4jGraph
+    })
+  },
+  false
+)
