94277dd10be95a9975f7e6c36fdbc7de2c5d06a4
Pierre Romera <hello@pirhoo.com>
2026-01-15 17:34:29 +0100

    build: install unplugin-icons

--- plugins/neo4j-graph-widget/vite.config.mjs
+++ plugins/neo4j-graph-widget/vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import { viteExternalsPlugin } from 'vite-plugin-externals'
 import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default ({ mode }) => {
@@ -19,7 +20,9 @@ export default ({ mode }) => {
         dts: false,
         dirs: [],
         resolvers: [BootstrapVueNextResolver()]
-      })
+      }),
+      // The "Icons" plugin generates icon components from Iconify collections.
+      Icons({ scale: 1, compiler: 'vue3' })
     ],
     resolve: {
       dedupe: ['vue'],
@@ -27,6 +30,9 @@ export default ({ mode }) => {
       alias: {
         '@': resolve(__dirname)
       }
+    },
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode)
     },
     build: {
       sourcemap: mode === 'development',
