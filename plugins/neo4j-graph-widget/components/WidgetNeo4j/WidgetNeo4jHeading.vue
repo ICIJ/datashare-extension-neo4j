6f0cdea1538c9e5480887d72928795a2c0e8c334
Pierre Romera <hello@pirhoo.com>
2026-01-15 17:35:21 +0100

    refactor: use kebab case

--- plugins/neo4j-graph-widget/components/WidgetNeo4j/WidgetNeo4jHeading.vue
+++ plugins/neo4j-graph-widget/components/WidgetNeo4j/WidgetNeo4jHeading.vue
@@ -12,7 +12,7 @@ defineProps({
 
 <template>
   <div class="d-flex gap-3 align-items-center mb-3">
-    <IPhPolygon size="2rem" />
+    <i-ph-polygon size="2rem" />
     <h4 class="p-0 m-0">Explore graph with Neo4j</h4>
     <widget-neo4j-status :status="status" />
   </div>
